dd809cdfa021ac676336f2a7823672fbc69dc432
dhruv552 <dhruvagrawal552@gmail.com>
2026-05-08 15:46:34 +0530

    models for project

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
+import mongoose, { Schema } from "mongoose";
 
 
 const userSchema = new mongoose.Schema(
