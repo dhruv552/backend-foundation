ed22f104044f9464a551d6dbca6732f24ea685d4
dhruv552 <dhruvagrawal552@gmail.com>
2026-05-11 15:56:02 +0530

    asdf

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,8 +1,57 @@
-import { User } from '../models/user.models.js';3
 import { apiResponse } from '../utils/api-response.js';
 import asyncHandler from "../utils/async-handler.js";
 import { apiError } from '../utils/api-error.js';
-import { emailVerificationMailgenContent, sendEmail } from '../utils/mail.js';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
+import { Project } from '../models/project.models.js';
+import { ProjectMember } from '../models/projectmember.models.js';
+import { User } from '../models/user.models.js';
+
+const getProject = asyncHandler(async (req, res) => {
+     // test
+})
+
+const getProjectById = asyncHandler(async (req, res) => {
+    // test
+})
+
+const createProject = asyncHandler(async (req, res) => {
+    // test
+})
+
+const updateProject = asyncHandler(async (req, res) => {
+    // test
+})
+
+const deleteProject = asyncHandler(async (req, res) => {
+    // test
+})
+
+const addMemberToProject = asyncHandler(async (req, res) => {
+    // test
+})
+
+const getProejctMembers = asyncHandler(async (req, res) => {
+    // test
+})
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+    // test
+})
+
+const deleteMemberRole = asyncHandler(async (req, res) => {
+    // test
+})
+
+export {
+    getProject,
+    getProjectById,
+    createProject,
+    updateProject,
+    deleteProject,
+    addMemberToProject,
+    getProejctMembers,
+    updateMemberRole,
+    deleteMemberRole,
+}
+
+
 
