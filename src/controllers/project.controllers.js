5fded8cefd23a00b83d40fc854c2b395ffd80524
dhruv552 <dhruvagrawal552@gmail.com>
2026-05-13 22:54:46 +0530

    first agreegation pipleline

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -74,7 +74,65 @@ const deleteProject = asyncHandler(async (req, res) => {
 })
 
 const getProject = asyncHandler(async (req, res) => {
-    // test
+    const projects = await ProjectMember.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(req.user._id),
+            }
+        },
+        {
+            $lookup: {
+                from: "projects",
+                localField: "project",
+                foreignField: "_id",
+                as: "projects",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "projectmembers",
+                            localField: "_id",
+                            foreignField: "project",
+                            as: "projectMembers",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            members: {
+                                $size: "$projectMembers",
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$projects",
+
+        },
+        {
+            $project: {
+                project:{
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy: 1,
+                },
+                role: 1,
+                _id: 0,
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            projects,
+            "Projects fetched successfully", 
+    )
+)
 })
 
 const getProjectById = asyncHandler(async (req, res) => {
